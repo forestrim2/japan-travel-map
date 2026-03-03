b0e4becaf7995760fc11d1ce5b093a307f1c7b93
forestrim2 <hlim949@gmail.com>
2026-03-03 09:34:16 +0900

    Delete src directory

--- src/utils/bounds.js
+++
@@ -1,7 +0,0 @@
-import L from "leaflet";
-
-// 한국/일본 커버 범위(대략) - 필요하면 미세 조정 가능
-export const KJ_BOUNDS = L.latLngBounds(
-  L.latLng(30.0, 122.0), // south, west
-  L.latLng(46.2, 146.8)  // north, east
-);
