90a4144c9774d170a04aa1360ea0a957015940b7
forestrim2 <hlim949@gmail.com>
2026-03-04 16:50:58 +0900

    Delete src directory

--- src/map/LocationButton.jsx
+++
@@ -1,1 +0,0 @@
-import React from 'react'; export default function LocationButton({onClick}){return <button className='fab secondary' title='현위치' onClick={onClick}>◎</button>;}
--- src/utils/confirm.js
+++
@@ -1,1 +0,0 @@
-export function confirmDelete(msg='정말 삭제하십니까?') { return window.confirm(msg); }
